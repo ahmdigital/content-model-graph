67c50029cee7b2d8e79e866f3b1171e6acb6238b
Cameron Chamberlain <git@camjc.com>
2019-11-30 11:37:22 +1100

    [patch] fix array of references

--- src/component/utils/get-nodes-from-types/index.test.js
+++ src/component/utils/get-nodes-from-types/index.test.js
@@ -1,23 +1,26 @@
 import { types } from '../../../fixtures';
-import getNodesFromTypes from "./";
+import getNodesFromTypes from './';
 
 const sharedOutput = [
   '"organization" [ label ="<root> Organization" shape="record" style=rounded]',
   '"discountCode" [ label ="<root> Discount Code" shape="record" style=rounded]',
-  '"urlObject" [ label ="<root> Url Object" shape="record" style=rounded]',
+  '"urlObject" [ label ="<root> Url Object" shape="record" style=rounded]'
 ];
 
 it('transforms types to nodes without showing fields', () => {
   const isShowingFields = false;
   const output = getNodesFromTypes(types, isShowingFields);
-  expect(output).toEqual(['"memberPerk" [ label ="<root> Member Perk" shape="record" style=rounded]', ...sharedOutput]);
+  expect(output).toEqual([
+    '"memberPerk" [ label ="<root> Member Perk" shape="record" style=rounded]',
+    ...sharedOutput
+  ]);
 });
 
 it('transforms types to nodes with showing fields', () => {
   const isShowingFields = true;
   const output = getNodesFromTypes(types, isShowingFields);
   expect(output).toEqual([
-    '"memberPerk" [ label ="<root> Member Perk|<offeredBy> Offered By|<urlObject> URL Object|<discountCodes> Discount Codes" shape="record" style=rounded]',
-    ...sharedOutput,
+    '"memberPerk" [ label ="<root> Member Perk|<offeredBy> Offered By|<urlObject> URL Object|<discountCodes> Discount Codes|<discountCodeReferences> Discount Code References" shape="record" style=rounded]',
+    ...sharedOutput
   ]);
 });
